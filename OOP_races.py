10d001a905f065755820f3afff56288ed53031ed
lPdFUzZT <thatelitemaili33t@gmail.com>
2021-05-10 14:54:30 +0300

    Added validators for the Car class attributes

--- OOP_races.py
+++ OOP_races.py
@@ -1,21 +1,72 @@
-class Car:
+from schema import Schema, And, Use, Optional, SchemaError
 
-    def __init__(self, driver: dict, wheels: list, engine: dict, electronics: dict) -> None:
+
+class Car:
+    engine_conf = {'turnovers': 200, 'fuel_supply': 200}
+    electronics_conf = {'OK': True}
+
+    def __init__(self, driver: dict, driver_conf: Schema, wheels: list,
+                 wheel_conf: Schema, engine: dict, engine_conf: Schema,
+                 electronics: dict, electronics_conf: Schema) -> None:
+
         if len(wheels) != 4:
             raise ValueError
+        if self._driver_data_validator(data=driver, conf=driver_conf) is True:
+            self.driver = driver
+        else:
+            raise AttributeError
 
-        self.driver = driver
-        self.wheels = wheels
-        self.engine = engine
-        self.electronics = electronics
+        if self._wheels_data_validator(lst=wheels, conf=wheel_conf) is True:
+            self.wheels = wheels
+        else:
+            raise AttributeError
 
-    def show_driver_info(self):
+        if self._engine_data_validator(data=engine, conf=engine_conf) is True:
+            self.engine = engine
+        else:
+            raise AttributeError
+
+        if self._electronics_data_validator(data=electronics, conf=electronics_conf) is True:
+            self.electronics = electronics
+        else:
+            raise AttributeError
+
+    def _driver_data_validator(self, data: dict, conf: Schema) -> bool:
+        try:
+            conf.validate(data)
+            return True
+        except SchemaError:
+            return False
+
+    def _engine_data_validator(self, data: dict, conf: Schema) -> bool:
+        try:
+            conf.validate(data)
+            return True
+        except SchemaError:
+            return False
+
+    def _electronics_data_validator(self, data: dict, conf: Schema) -> bool:
+        try:
+            conf.validate(data)
+            return True
+        except SchemaError:
+            return False
+
+    def _wheels_data_validator(self, lst: list, conf: Schema) -> bool:
+        for wheel in lst:
+            try:
+                conf.validate(wheel)
+                return True
+            except SchemaError:
+                return False
+
+    def show_driver_info(self) -> str:
         return f" Driver's card\n " \
                f"Name: {self.driver.get('name')} , " \
                f"Skills rate: {self.driver.get('skills')} ," \
                f"Age: {self.driver.get('age')}"
 
-    def show_wheels_info(self):
+    def show_wheels_info(self) -> str:
         s = "Wheels data \n"
         for wheel in self.wheels:
             s += f"Wheel brand: {wheel.get('wheel_brand')} ," \
@@ -24,39 +75,38 @@ class Car:
 
         return s
 
-    def show_engine_info(self):
+    def show_engine_info(self) -> str:
         return f" Engine data\n " \
                f"Turnovers: {self.engine.get('turnovers')} ," \
                f"Fuel supply: {self.engine.get('fuel_supply')}/1000"
 
-    def show_electronics_info(self):
+    def show_electronics_info(self) -> str:
         return f" Electronics data\n " \
                f"OK: {self.electronics.get('OK')} "
 
 
-c = Car(driver={'skills': 0, 'name': 'John Johnson', 'age': 18}, wheels=[{'wheel_brand': 'Michelin',
-                                                                          'diameter': 20, 'quality': 1},
-                                                                         {'wheel_brand': 'Michelin',
-                                                                          'diameter': 20, 'quality': 1},
-                                                                         {'wheel_brand': 'Michelin',
-                                                                          'diameter': 20, 'quality': 1},
-                                                                         {'wheel_brand': 'Michelin',
-                                                                          'diameter': 20, 'quality': 1}],
-        engine={'turnovers': 200, 'fuel_supply': 200},
-        electronics={'OK': True})
+c = Car(driver={'skills': 0, 'name': 'John Johnson', 'age': 18},
+        driver_conf=Schema({'skills': And(Use(int)), 'name': And(Use(str)), 'age': And(Use(int))}), wheels=[
+                                                                      {'wheel_brand': 'Michelin',
+                                                                       'diameter': 20, 'quality': 1},
+                                                                      {'wheel_brand': 'Michelin',
+                                                                       'diameter': 20, 'quality': 1},
+                                                                      {'wheel_brand': 'Michelin',
+                                                                       'diameter': 20, 'quality': 1},
+                                                                      {'wheel_brand': 'Michelin',
+                                                                       'diameter': 20, 'quality': 1}],
+        wheel_conf=Schema({'wheel_brand': And(Use(str)), 'diameter': And(Use(int)), 'quality': And(Use(int))}),
+        engine={'turnovers': 200, 'fuel_supply': 200}, engine_conf=Schema({'turnovers': And(Use(int)),
+                                                                           'fuel_supply': And(Use(int))}),
+        electronics={'OK': True}, electronics_conf=Schema({'OK': And(Use(bool))}))
+
 
 print(c.show_driver_info())
 print(c.show_wheels_info())
 print(c.show_engine_info())
 print(c.show_electronics_info())
 
-
 """
 stuff for the  future
 """
-# if not driver:
-#     driver = {'skills': 0, 'name': None, 'age': 18}
-
-# if isinstance(driver, dict) and isinstance(conf, dict):
-# struct is a dict of types or other dicts
-# return all(k in conf and check_structure(struct[k], conf[k]) for k in struct
+# class Road
