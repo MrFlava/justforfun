aac42d63d0338dc4c6051084f778feeb3fcd2a82
lPdFUzZT <thatelitemaili33t@gmail.com>
2021-08-07 17:05:25 +0300

    Updated some_fun_stuff.py

--- some_fun_stuff.py
+++ some_fun_stuff.py
@@ -1,42 +1,50 @@
 # 1. Palindrome in list checking
 
+#
 # def palindromes_check(words: list) -> list:
 #     return list(filter(lambda s: s[::-1] == s, words))
+#
+
 # 2. Decorator benchmark
 
 # import time
 # import functools
-
+#
+#
 # class myShinyDeco:
-#     def __init__(self, fmt='completed {:s} in {:.3f} seconds'):
-#             self.fmt = fmt
-
+#     def __init__(self, fmt="completed {:s} in {:.3f} seconds"):
+#         self.fmt = fmt
+#
 #     def __call__(self, fn):
 #         @functools.wraps(fn)
 #         def wrapper(*args, **kwargs):
-#           t1 = time.time()
-#           res = fn(*args, **kwargs)
-#           t2 = time.time()
-#           print(self.fmt.format(fn.__name__, t2-t1))
-#           return res
+#             t1 = time.time()
+#             res = fn(*args, **kwargs)
+#             t2 = time.time()
+#             print(self.fmt.format(fn.__name__, t2 - t1))
+#             return res
+#
 #         return wrapper
-
+#
+#
 # def calling_counter(f):
 #     def wrapper(*args, **kwargs):
 #         wrapper.calls += 1
-#         print(f'Decorator was called : {wrapper.calls}  times')
+#         print(f"Decorator was called : {wrapper.calls}  times")
 #         return f(*args, **kwargs)
+#
 #     wrapper.calls = 0
 #     return wrapper
-
+#
+#
 # @myShinyDeco()
 # @calling_counter
 # def func():
-#     return 'coolest function across USA '
+#     return "coolest function across USA "
+
 
 # 3. Delete duplicates with two ways
 """ First way """
-
 
 # def delete_duplicates_way_two(strings_list: list) -> list:
 #     return list(set(strings_list))
@@ -45,6 +53,7 @@
 """ Second way """
 
 
+#
 # def delete_duplicates_way_one(strings_list: list) -> list:
 #     output_list = []
 #     for i in strings_list:
@@ -52,38 +61,128 @@
 #             output_list.append(i)
 #     return output_list
 
+
 # 4. Operations with string numbers
 
+
 # def str_sum(num1: str, num2: str, oper: str) -> float:
-#     if oper not in ['+', '-', '/', '*']:
+#     if oper not in ["+", "-", "/", "*"]:
 #         raise NotImplementedError
 #
 #     operators = {
-#         '+': (lambda: int(num1) + int(num2))(),
-#         '-': (lambda: int(num1) - int(num2))(),
-#         '*': (lambda: int(num1) * int(num2))(),
-#         '/': (lambda: int(num1) / int(num2))(),
+#         "+": (lambda: int(num1) + int(num2))(),
+#         "-": (lambda: int(num1) - int(num2))(),
+#         "*": (lambda: int(num1) * int(num2))(),
+#         "/": (lambda: int(num1) / int(num2))(),
 #     }
 #
 #     return operators[oper]
 
-# def main():
-# num1 = '555'
-# num2 = '888'
-# operator = '+'
 
-# a = ['mega', 'level', 'pop', 'funny', 'wave', 'cool', 'super', 'mom']
-# s_list = ['bill', 'there', 'hey', 'pay', 'bill', 'there', 'sugar']
-
-# print(str_sum(num1, num2, operator))
-# print(delete_duplicates_way_one(s_list))
-# print(delete_duplicates_way_two(s_list))
-
-# print(func())
-
-# print(palindromes_check(a))
-
-# if __name__ == "__main__":
-#     main()
+# 5. Iterator with Class
 
 
+# class Count:
+#     def __init__(self, start):
+#         self.num = start
+#         self.limit = 1000
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         num = self.num
+#         self.num += 1
+#         if not self.num >= self.limit:
+#             return num
+#         else:
+#             raise ValueError
+
+
+# 6. Python generator
+# def simple_generator(val: str):
+#     if isinstance(val, str):
+#         while len(val) > 0:
+#             val += "a"
+#             yield val
+#     raise TypeError
+
+
+# 7. Backpack task ( dynamic programming )
+
+# from argparse import ArgumentParser
+#
+# parser = ArgumentParser(description="Task 2 Exercise 1")
+# parser.add_argument("-W", nargs=1, type=int, help="capacity")
+# parser.add_argument("-w", nargs="*", type=int, help="weights")
+# parser.add_argument("-n", nargs=1, type=int, help="bars_number")
+#
+#
+# def max_gold(c, w, n):
+#     K = [[0 for x in range(c + 1)] for x in range(n + 1)]
+#
+#     for i in range(n + 1):
+#         for j in range(c + 1):
+#             if i == 0 or j == 0:
+#                 K[i][j] = 0
+#             elif w[i - 1] <= j:
+#                 K[i][j] = max(w[i - 1] + K[i - 1][j - w[i - 1]], K[i - 1][j])
+#             else:
+#                 K[i][j] = K[i - 1][j]
+#
+#     return K[n][c]
+#
+#
+# def bounded_knapsack(args):
+#
+#     args_dict = vars(args)
+#     capacity = args_dict["W"][0]
+#     weights = args_dict["w"]
+#     bars_number = args_dict["n"][0]
+#
+#     for w in weights:
+#         if w < 0:
+#             raise ValueError
+#
+#     if len(weights) != bars_number or capacity < 0:
+#         raise ValueError
+#     else:
+#         return max_gold(capacity, weights, bars_number)
+
+
+def main():
+    pass
+    # args = parser.parse_args()
+    #
+    # print(bounded_knapsack(args))
+
+    # new_generator = simple_generator("b")
+    #
+    # print(next(new_generator))
+    # print(next(new_generator))
+    # print(next(new_generator))
+    # print(next(new_generator))
+    # print(next(new_generator))
+    #
+    # c = Count(-100)
+    # for i in c:
+    #     print(i)
+
+    # num1 = "555"
+    # num2 = "888"
+    # operator = "+"
+    #
+    # a = ["mega", "level", "pop", "funny", "wave", "cool", "super", "mom"]
+    # s_list = ["bill", "there", "hey", "pay", "bill", "there", "sugar"]
+    #
+    # print(str_sum(num1, num2, operator))
+    # print(delete_duplicates_way_one(s_list))
+    # print(delete_duplicates_way_two(s_list))
+    #
+    # print(func())
+    #
+    # print(palindromes_check(a))
+
+
+if __name__ == "__main__":
+    main()
